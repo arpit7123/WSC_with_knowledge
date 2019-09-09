310eab7419b1377c8794d93e719b17649a286bdf
Ashok Prakash <pashok.ash@gmail.com>
2019-09-09 15:59:28 -0700

    QASRL outputs

--- scripts/entailment_demo.py
+++ scripts/entailment_demo.py
@@ -2,6 +2,8 @@ from allennlp.predictors.predictor import Predictor
 from allennlp.predictors import Predictor
 from allennlp.models.archival import load_archive
 import json
+import nltk
+import re 
 
 #model = PretrainedModel('./esim-elmo-2018.05.17.tar.gz','textual-entailment')
 #predictor = model.predictor()
@@ -44,7 +46,6 @@ def rank_knowledge():
             obj.append(o)
         obj.sort(key=lambda x: x['score'], reverse=True)
         each['knowledge'] = obj
-        length = 10
         if(len(obj) > 10):
             new_obj = obj[0:10]
         else:
@@ -60,39 +61,69 @@ def rank_knowledge():
     with open('../data_sets/winogrande/knowledge_queries.json', 'w') as outfile:
         json.dump(knowledge, outfile)
 
-
-if __name__=="__main__":
+def create_files_for_qasrl():
     problems = "../data_sets/winogrande/knowledge_queries.json"
     f = open(problems, "r")
     all_probs = f.read()
     knowledge = json.loads(all_probs)
     
-    knowledge_qasrl = "../data_sets/winogrande/QASRL/winogrande_knowledge_score_290.txt"
-    f1 = open(knowledge_qasrl, "r")
-    knowledge_qasrl_txt = f1.read()
-    knowledge_qasrl_out = knowledge_qasrl_txt.split('\n')
-    start = 0
-    for i in range(0, len(knowledge)):
-        if(i == 290):
-            break
+    file_name = '../data_sets/winogrande/QASRL/winogrande_knowledge_score_323.txt'
+    for i in range(323, len(knowledge)):
         each = knowledge[i][0]
-        length = 10
-        if(len(each['knowledge']) < 10):
-            length = len(each['knowledge'])
-            
-        knowledge_objects = []
-        for j in range(start, len(knowledge_qasrl_out)):
-            if(j >= (start + length)):
-                start = start + length
+        for j in range(0, len(each['knowledge'])):
+            if(j == 10):
                 break
-            json_obj = json.loads(knowledge_qasrl_out[j])
-            new_obj = {}
-            new_obj['k'] = json_obj['sentence']
-            knowledge_objects.append(new_obj)
-        each['knowledge'] = knowledge_objects
+            k = each['knowledge'][j]
+            obj_sent =  {}
+            obj_sent['sentence'] = k['k']
+            #if(limit == 0):
+            #    limit = 2900
+            #    file_name = '../data_sets/winogrande/QASRL/winogrande_knowledge_score_'+str(i)+'.txt'
+            with open(file_name, 'a') as outfile:
+                json.dump(obj_sent, outfile)
+                outfile.write('\n')
+  
+def Find(string): 
+    # findall() has been used  
+    # with valid conditions for urls in string 
+    url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string) 
+    return url
+
+def clean_knowledge_data_set():
+    problems = "../data_sets/winogrande/knowledge_queries.json"
+    f = open(problems, "r")
+    all_probs = f.read()
+    knowledge = json.loads(all_probs)
     
+    for i in range(0, len(knowledge)):
+        each = knowledge[i][0]
+        found= False 
+        output_k = []
+        for j in range(0, len(each['knowledge'])):
+            k = each['knowledge'][j]
+            urls = Find(k['k'])
+            for u in urls:
+                k['k'] = k['k'].replace(u, '')
+            tokens = nltk.word_tokenize(k['k'].lower())
+            text = nltk.Text(tokens)
+            tagged = nltk.pos_tag(text)
+            found = False
+            if(len(tokens) <  7):
+                continue
+            for word,tag in tagged:
+                if('VB' in  tag):
+                    found = True
+                    break
+            if(found):
+                output_k.append(k)
+        each['knowledge'] = output_k
+           
     with open('../data_sets/winogrande/knowledge_queries.json', 'w') as outfile:
-        json.dump(knowledge, outfile)
+       json.dump(knowledge, outfile)
+    
+
+if __name__=="__main__":
+   create_files_for_qasrl()
             
             
         
